972ed05bd98202ec23fe244a576f6cdab826f84d
meghanaanjali <meghanaanjali@gmail.com>
2018-05-29 08:32:44 +0530

    added bookmark model

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Link
+from .models import Bookmark,Link
 
 admin.site.register(Link)
+admin.site.register(Bookmark)
--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Link(models.Model):
@@ -5,3 +6,13 @@ class Link(models.Model):
 
     def __str__(self):
         return self.url
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    link = models.ForeignKey(Link, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '''title: {0}, user:{1}, link: {2}'''.format(self.title,
+                                                     self.user,
+                                                     self.link )               
